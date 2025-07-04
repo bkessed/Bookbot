237b3c53306395a0324e3785ffe3704f20648cdd
Bless <blackflug3@gmail.com>
2025-07-05 02:04:41 +0300

    Completely finished project

--- main.py
+++ main.py
@@ -2,17 +2,21 @@ from stats import words_count
 from stats import symbol_count
 from stats import sort_on
 from stats import symbol_sorted_list
+import sys
 
 def get_book_text(filepath):
     with open(filepath) as f:
         return f.read()
 
 def main():
-    filepath = "books/frankenstein.txt"
+    if len(sys.argv) != 2:
+        print("Usage: python3 main.py <path_to_book>")
+        sys.exit(1)
+    filepath = sys.argv[1]
     book_text = get_book_text(filepath)
     stats = symbol_count(book_text)
     print("============ BOOKBOT ============")
-    print("Analyzing book found at books/frankenstein.txt...")
+    print(f"Analyzing book found at {sys.argv[1]}")
     print("----------- Word Count ----------")
     print(f"Found {words_count(book_text)} total words")
     print("--------- Character Count -------")
